7c9248660a90f3a66ca7a598af2393e08347ce62
Gustavo Ovelar <gusove@gmail.com>
2017-08-06 22:04:34 -0300

    Creiaçaõ de Tela principal do Sistema com Barra de Menu aplicando CSS 3 com HTML

--- Escola/App_Start/BundleConfig.cs
+++ Escola/App_Start/BundleConfig.cs
@@ -26,7 +26,9 @@ namespace Escola
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/nav.css",
+                      "~/Content/ie.css"));
         }
     }
 }
